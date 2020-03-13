ac5d7bd08a22f3be71b7df94353072d6b789ebcf
s18788 <s18788@pjwstk.edu.pl>
2020-03-13 17:23:01 +0100

    commit

---
+++ Cw_02/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Cw_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string path = @"C:\\College\\C#\\Cw2";
+       
+
+            //Wczytywanie 
+            var file = new FileInfo(path);
+            using (var stream = new StreamReader(file.OpenRead()))
+            {
+                string line = null;
+                while ((line = stream.ReadLine()) != null)
+                {
+                    string[] kolumny = line.Split(',');
+                    Console.WriteLine(line);
+                }
+            }
+            //stream.Dispose();
+
+            //XML
+            var list = new List<Student>();
+            var st = new Student("Jan",
+                "Kowalski",
+                "kowalski@wp.pl");
+            list.Add(st);
+
+            FileStream writer = new FileStream(@"data.xml", FileMode.Create);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Student>),
+                                       new XmlRootAttribute("uczelnia"));
+            serializer.Serialize(writer, list);
+            serializer.Serialize(writer, list);
+
+
+
+        }
+    }
+}
---
+++ Cw_02/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Student
+{
+	private string imie;
+	private string email;
+	private string nazwisko;
+
+	public Student(string imie,string email, string nazwisko )
+	{
+		this.imie = imie;
+		this.nazwisko = nazwisko;
+		this.email = email;
+
+
+
+
+	}
+}
